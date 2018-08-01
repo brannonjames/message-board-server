8aaf06d8d986dc351c1d8b00f5c64cd32c4cb69c
Jimmy Brannon <user.email>
2018-08-01 16:23:41 -0500

    update env variables

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const app = require('express')();
-const server = app.listen(8000, () => console.log('server running'));
+const PORT = process.env.PORT || 8000;
+const server = app.listen(PORT, () => console.log('server running'));
 const io = require('socket.io')(server);
 const socketSetup = require('./services/socketio.js');
 const mongoose = require('mongoose');
 
 mongoose.set('degub', process.env.DB_DEBUG || false);
-mongoose.connect('mongodb://localhost:27017/message-board', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/message-board', { useNewUrlParser: true });
 mongoose.Promise = Promise;
 
 socketSetup(io);
