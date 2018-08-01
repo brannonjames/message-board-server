d9947286292c03a34cf18d2271f02a3989bf791b
Jimmy Brannon <user.email>
2018-08-01 16:38:18 -0500

    update imports

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const app = require('express')();
 const PORT = process.env.PORT || 8000;
-const server = app.listen(PORT, () => console.log('server running'));
-const io = require('socket.io')(server);
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
 const socketSetup = require('./services/socketio.js');
 const mongoose = require('mongoose');
+
+server.listen(PORT, () => console.log('server running'));
 
 mongoose.set('degub', process.env.DB_DEBUG || false);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/message-board', { useNewUrlParser: true });
